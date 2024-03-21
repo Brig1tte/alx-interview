9793c5c332b3d1cd7d52dcf713eeb2b9c09c668a
Brig1tte <korantengbrigitte96@gmail.com>
2024-03-21 23:54:10 +0000

    MinOperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Bash script to execute only two operations in this file: Copy All and
+Paste. If n is impossible to achieve, return 0 else return an Integer """
+
+def minOperations(n: int) -> int:
+    """ Funct to calculate the least number of opreations needed to
+    result in exactly "n" "H" characters """
+
+    operations: int = 0
+    divs: int = 2
+
+    if n <= 1:
+        return 0
+    while n > 1:
+        if n % divs == 0:
+            operations += divs
+            n /= divs
+        else:
+            divs += 1
+
+    return operations
