146192678967d52deae3dcdd705d30f92fbb6fb2
Brig1tte <korantengbrigitte96@gmail.com>
2024-05-16 01:01:38 -0700

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,16 +2,23 @@
 
 const request = require('request');
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
-  if (err) throw err;
-  const actors = JSON.parse(body).characters;
-  exactOrder(actors, 0);
-});
-const exactOrder = (actors, x) => {
-  if (x === actors.length) return;
-  request(actors[x], function (err, res, body) {
+const movieId = process.argv[2];
+
+const printCharacterNames = (characterUrls, index) => {
+  if (index === characterUrls.length) return;
+  
+  request(characterUrls[index], function (err, res, body) {
     if (err) throw err;
-    console.log(JSON.parse(body).name);
-    exactOrder(actors, x + 1);
+    const characterData = JSON.parse(body);
+    console.log(characterData.name);
+    printCharacterNames(characterUrls, index + 1);
   });
 };
+
+request(`https://swapi-api.hbtn.io/api/films/${movieId}`, function (err, res, body) {
+  if (err) throw err;
+  
+  const movieData = JSON.parse(body);
+  const characterUrls = movieData.characters;
+  printCharacterNames(characterUrls, 0);
+});
